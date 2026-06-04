12bcd173435ff5da821ec4853444cdf8673d7d8a
WolfTechAustria <wo@wolftech.at>
2026-06-04 22:25:31 +0200

    add DeviceModel

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -3,16 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Device extends Model
 {
     protected $fillable = [
+        'uuid',
         'name',
         'fingerprint',
         'platform',
         'app_version',
+        'last_seen_at',
         'approved_at',
-        'active',
-        'api_token',
+        'approved_by',
+        'status',
     ];
+
+    protected $casts = [
+        'last_seen_at' => 'datetime',
+        'approved_at' => 'datetime',
+    ];
+
+    public function approver(): BelongsTo
+    {
+        return $this->belongsTo(
+            User::class,
+            'approved_by'
+        );
+    }
 }
