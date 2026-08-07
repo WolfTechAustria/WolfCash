bea94e2c2ee5492a58dded35ad44601226686d6d
WolfTechAustria <wo@wolftech.at>
2026-08-07 21:39:49 +0200

    changed bootstrap app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'device.approved' => \App\Http\Middleware\EnsureDeviceIsApproved::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
