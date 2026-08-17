1cf710fdbbb6485ff614f67f2f7bb2f5afc5f214
WolfTechAustria <wo@wolftech.at>
2026-08-17 22:33:45 +0200

    added mobile web url

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'mobile_web_url' => env('MOBILE_WEB_URL',env('APP_URL')),
 
     /*
     |--------------------------------------------------------------------------
